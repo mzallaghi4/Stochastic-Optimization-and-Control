9b9de784ee3a01f3a38813228f43cf73854a00df
Mehdi Zallaghi <mmzallaghi@gmail.com>
2025-09-30 21:13:31 +0330

    Update RL_Trading.py

--- ReinforcementLearning/RL_Trading.py
+++ ReinforcementLearning/RL_Trading.py
@@ -1,1 +1,11 @@
+### RL for Quantitative Trading
+import gym
+import numpy as np
+import pandas as pd
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import random
+from collections import deque
+import matplotlib.pyplot as plt
 
